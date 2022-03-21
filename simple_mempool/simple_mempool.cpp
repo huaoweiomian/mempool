b8e2839e329ba4560f9f9f657bc460d332430198
huaoweiomian <huaoweiomian@163.com>
2022-03-21 13:38:07 +0800

    add simple memory pool

---
+++ simple_mempool/simple_mempool.cpp
@@ -0,0 +1,6 @@
+#include "simple_mempool.h"
+
+SIMPLE_MEMPOOL::SIMPLE_MEMPOOL()
+{
+
+}
---
+++ simple_mempool/simple_mempool.h
@@ -0,0 +1,11 @@
+#ifndef SIMPLE_MEMPOOL_H
+#define SIMPLE_MEMPOOL_H
+
+
+class SIMPLE_MEMPOOL
+{
+public:
+    SIMPLE_MEMPOOL();
+};
+
+#endif // SIMPLE_MEMPOOL_H
